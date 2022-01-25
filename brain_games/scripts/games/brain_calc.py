e73a93ebe78f382e029b8b23c384b9b41c9ed4d0
Xisp_93 <voevoda43@gmail.com>
2022-01-25 17:28:09 +0400

    Исправление в brain_calc.py

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -15,7 +15,7 @@ def main():
         operat_res = random.choice(operat)
         num_one = random.randrange(0, rang)
         num_two = random.randrange(0, rang)
-        print('Question:{0}{1}{2}'.format(num_one, operat_res, num_two))
+        print('Question: {0}{1}{2}'.format(num_one, operat_res, num_two))
         if operat_res == '+':
             res = num_one + num_two
         elif operat_res == '-':
